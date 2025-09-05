8b6d3640624232fb86bf9e925557448d1c210de8
QuikeLRD <enriquelerdo010@hotmail.com>
2025-09-05 17:00:09 -0600

    PRACTICA1 EJERCICIO1

---
+++ PRACTICA1/PRACTICA1/main.c
@@ -0,0 +1,34 @@
+/*
+ * main.c
+ *
+ * Created: 9/5/2025 4:45:44 PM
+ *  Author: enriq
+ */ 
+
+#define  CPU_F1000000
+#include <xc.h>
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int i= 0xF0, j = 0b10101010, y = 85;
+
+
+int main(void)
+{
+	
+	DDRA = 0x00;
+	DDRB = 255;
+	DDRC = 0xFF;
+	
+    while(1)
+    {
+        PORTB = j;
+		PORTC = y;
+		_delay_ms(500);
+		PORTB = y;
+		PORTC = j;
+		_delay_ms(500);
+		
+    }
+}
