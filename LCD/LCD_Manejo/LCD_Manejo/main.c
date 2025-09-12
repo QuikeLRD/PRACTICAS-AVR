57b7b04551fb484bc537602b28139cc094a7c19f
Quike <enriquelerdo010@hotmail.com>
2025-09-12 14:25:02 -0600

    DISPAROS

--- LCD/LCD_Manejo/LCD_Manejo/main.c
+++ LCD/LCD_Manejo/LCD_Manejo/main.c
@@ -5,7 +5,7 @@
 #include "LCD.h" 
 
 char mensaje[] = "Bienvenido, a";
-char mensaje2[] = "Plantas vs Zombis";
+char mensaje2[] ="PlantasVS Zombis";
 
 
 int main(void)
@@ -56,20 +56,20 @@ int main(void)
 		POS_LINEA1(15);
 		ENVIA_DATO(5);
 		_delay_ms(500);
+		
 		//BORRO ZOMBI1
 		POS_LINEA1(15);
 		ENVIA_DATO(' ');  // Escribe espacio para borrar
 		_delay_ms(10);
-				
 		//ZOMBI P1
 		POS_LINEA1(14);
 		ENVIA_DATO(5);
 		_delay_ms(500);
+		
 		//BORRO ZOMBI P1
 		POS_LINEA1(14);
 		ENVIA_DATO(' ');  
 		_delay_ms(10);
-		
 		//ZOMBI1 P2
 		POS_LINEA1(13);
 		ENVIA_DATO(5);
@@ -87,7 +87,7 @@ int main(void)
 		ENVIA_DATO(5);
 		_delay_ms(500);
 				
-		//DISPARO
+		//DISPARO 1
 		POS_LINEA1(2);
 		ENVIA_DATO(4);
 		//BORRO ZOMBI1 P3
@@ -99,7 +99,11 @@ int main(void)
 		ENVIA_DATO(5);
 		_delay_ms(500);
 		
-		//DISPARO
+		//BORRO DISPARO1
+		POS_LINEA1(2);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//DISPARO 2
 		POS_LINEA1(3);
 		ENVIA_DATO(4);
 		//BORRO ZOMBI1 P4
@@ -111,27 +115,34 @@ int main(void)
 		ENVIA_DATO(5);
 		_delay_ms(500);
 		
-		//DISPARO
+		
+		//BORRO DISPARO2
+		POS_LINEA1(3);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//DISPARO 3
 		POS_LINEA1(4);
 		ENVIA_DATO(4);
 		//BORRO ZOMBI1 P4
 		POS_LINEA1(10);
 		ENVIA_DATO(' ');
 		_delay_ms(10);
-		
 		//ZOMBI2 P1
 		POS_LINEA2(15);
 		ENVIA_DATO(7);
-		
 		//ZOMBI P6
 		POS_LINEA1(9);
 		ENVIA_DATO(5);
 		_delay_ms(500);
 				
-		//DISPARO
+		
+		//BORRO DISPARO3
+		POS_LINEA1(4);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//DISPARO4
 		POS_LINEA1(5);
 		ENVIA_DATO(4);
-		
 		//BORRO ZOMBI P2
 		POS_LINEA1(9);
 		ENVIA_DATO(' ');
@@ -143,7 +154,6 @@ int main(void)
 		//ZOMBI P3
 		POS_LINEA1(8);
 		ENVIA_DATO(5);
-		
 		//ZOMBI2 P1
 		POS_LINEA2(14);
 		ENVIA_DATO(7);		
@@ -161,7 +171,11 @@ int main(void)
 		//MUEVO ZOMBI1 P4
 		POS_LINEA1(7);
 		ENVIA_DATO(5);	
-		//DISPARO
+		//BORRO DISPARO4
+		POS_LINEA1(5);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//DISPARO 5
 		POS_LINEA1(6);
 		ENVIA_DATO(4);
 		//BORRO ZOMBI P4
@@ -176,12 +190,10 @@ int main(void)
 		//ZOMBI1 Golpeado
 		POS_LINEA1(6);
 		ENVIA_DATO(2);
-		
 		//BORRAR ZOMBI1
 		POS_LINEA1(7);
 		ENVIA_DATO(' ');
 		_delay_ms(10);
-		
 		//DISPARO PLANTA2
 		POS_LINEA2(2);
 		ENVIA_DATO(4);
@@ -197,16 +209,8 @@ int main(void)
 		//MUERE ZOMBI1
 		POS_LINEA1(6);
 		ENVIA_DATO(' ');
-		//BORRAMOS PROYECTILES PLANTA1
-		POS_LINEA1(2);
-		ENVIA_DATO(' ');
-		POS_LINEA1(3);
-		ENVIA_DATO(' ');
-		POS_LINEA1(4);
-		ENVIA_DATO(' ');
-		POS_LINEA1(5);
-		ENVIA_DATO(' ');
-		POS_LINEA1(6);
+		//BORRO DISPARO1 PLANTA 2
+		POS_LINEA2(2);
 		ENVIA_DATO(' ');
 		_delay_ms(10);
 		//DISPARO PLANTA2
@@ -231,9 +235,19 @@ int main(void)
 		//MOVEMOS ZOMBI 2 (10)
 		POS_LINEA2(10);
 		ENVIA_DATO(7);
-		//DISPARO PLANTA2
+		//BORRO DISPARO2 PLANTA2
+		POS_LINEA2(3);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//DISPARO3  PLANTA2
 		POS_LINEA2(4);
 		ENVIA_DATO(4);
+		_delay_ms(500);
+		
+		
+		//ZOMBI DE CUBETA
+		POS_LINEA2(14);
+		ENVIA_DATO(1);
 		//BORRAMOS ZOMBI 2 (9)
 		POS_LINEA2(10);
 		ENVIA_DATO(' ');
@@ -241,20 +255,299 @@ int main(void)
 		//MOVEMOS ZOMBI 2 (11)
 		POS_LINEA2(9);
 		ENVIA_DATO(7);
+		//BORRO DISPARO3 PLANTA2
+		POS_LINEA2(4);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
 		//DISPARO PLANTA2
+		POS_LINEA2(5);
+		ENVIA_DATO(4);
+		_delay_ms(500);
+		
+		
+		//ZOMBI DE CUBETA 2
+		POS_LINEA1(14);
+		ENVIA_DATO(1);
+		//BORRO DISPARO3 PLANTA2
+		POS_LINEA2(5);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//DISPARO PLANTA2
+		POS_LINEA2(6);
+		ENVIA_DATO(4);
+		//BORRAMOS ZOMBI 2 (9)
+		POS_LINEA2(9);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//MOVEMOS ZOMBI 2 (11)
+		POS_LINEA2(8);
+		ENVIA_DATO(7);
+		//BORRAMOS ZOMBI DE CUBETA (1)
+		POS_LINEA2(14);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//MOVEMOS ZOMBI DE CUBETA
+		POS_LINEA2(13);
+		ENVIA_DATO(1);
+		_delay_ms(500);
+		
+		//NUEVO LANZA GUISANTES
+		POS_LINEA2(2);
+		ENVIA_DATO(0);
+		//BORRO DISPARO PLANTA2
+		POS_LINEA2(6);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//DISPARO PLANTA2
+		POS_LINEA2(7);
+		ENVIA_DATO(4);
+		//BORRAMOS ZOMBI 2 (8)
+		POS_LINEA2(8);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//MOVEMOS ZOMBI 2 (11)
+		POS_LINEA2(7);
+		ENVIA_DATO(5);
+		//BORRAMOS ZOMBI DE CUBETA (1)
+		POS_LINEA2(13);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//MOVEMOS ZOMBI DE CUBETA
+		POS_LINEA2(12);
+		ENVIA_DATO(1);
+		//BORRAMOS ZOMBI DE CUBETA2 
+		POS_LINEA1(14);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//MOVEMOS ZOMBI DE CUBETA 2
+		POS_LINEA1(13);
+		ENVIA_DATO(1);
+		_delay_ms(500);
+		
+		//DISPARO PLANTA3
+		POS_LINEA2(3);
+		ENVIA_DATO(4);
+		//BORRAMOS ZOMBI 2 (7)
+		POS_LINEA2(7);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//MOVEMOS ZOMBI 2 (6)
+		POS_LINEA2(6);
+		ENVIA_DATO(5);
+		//BORRAMOS ZOMBI DE CUBETA (2)
+		POS_LINEA2(12);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//MOVEMOS ZOMBI DE CUBETA
+		POS_LINEA2(11);
+		ENVIA_DATO(1);
+		//BORRAMOS ZOMBI DE CUBETA2
+		POS_LINEA1(13);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//MOVEMOS ZOMBI DE CUBETA 2
+		POS_LINEA1(12);
+		ENVIA_DATO(1);
+		_delay_ms(500);
+		
+		//DISPARO PLANTA3
 		POS_LINEA2(4);
 		ENVIA_DATO(4);
-			
+		//BORRAMOS ZOMBI 2 (6)
+		POS_LINEA2(6);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//MOVEMOS ZOMBI 2 (5)
+		POS_LINEA2(5);
+		ENVIA_DATO(5);
+		//BORRAMOS ZOMBI DE CUBETA (3)
+		POS_LINEA2(11);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//MOVEMOS ZOMBI DE CUBETA
+		POS_LINEA2(10);
+		ENVIA_DATO(1);
+		//BORRAMOS ZOMBI DE CUBETA2
+		POS_LINEA1(12);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//MOVEMOS ZOMBI DE CUBETA 2
+		POS_LINEA1(11);
+		ENVIA_DATO(1);
+		_delay_ms(500);
+		
+		//BORRAMOS DISPARO PLANTA 2
+		POS_LINEA1(3);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//BORRAMOS ZOMBI 2 (5)
+		POS_LINEA2(5);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//MOVEMOS ZOMBI 2 (4)
+		POS_LINEA2(4);
+		ENVIA_DATO(2);
+		//BORRAMOS ZOMBI DE CUBETA (4)
+		POS_LINEA2(10);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//MOVEMOS ZOMBI DE CUBETA
+		POS_LINEA2(9);
+		ENVIA_DATO(1);
+		//BORRAMOS ZOMBI DE CUBETA2
+		POS_LINEA1(11);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//MOVEMOS ZOMBI DE CUBETA 2
+		POS_LINEA1(10);
+		ENVIA_DATO(1);
+		_delay_ms(500);
+		
+		//MUERE ZOMBI
+		POS_LINEA2(4);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//BORRAMOS DISPARO PLANTA 2
+		POS_LINEA2(3);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//DISPARA PLANTA
+		POS_LINEA2(4);
+		ENVIA_DATO(4);
+		//DISPARA PLANTA
+		POS_LINEA1(2);
+		ENVIA_DATO(4);
+		//BORRAMOS ZOMBI DE CUBETA (5)
+		POS_LINEA2(9);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//MOVEMOS ZOMBI DE CUBETA
+		POS_LINEA2(8);
+		ENVIA_DATO(1);
+		//BORRAMOS ZOMBI DE CUBETA2
+		POS_LINEA1(10);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//MOVEMOS ZOMBI DE CUBETA 2
+		POS_LINEA1(9);
+		ENVIA_DATO(1);
+		_delay_ms(500);
+		
+		//BORRAMOS DISPARO PLANTA 2
+		POS_LINEA2(4);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//DISPARA PLANTA
+		POS_LINEA2(5);
+		ENVIA_DATO(4);
+		//BORRAMOS DISPARO PLANTA 4
+		POS_LINEA1(2);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//DISPARA PLANTA
+		POS_LINEA1(3);
+		ENVIA_DATO(4);
+		//DISPARA PLANTA1
+		POS_LINEA2(3);
+		ENVIA_DATO(4);
+		//BORRAMOS ZOMBI DE CUBETA (6)
+		POS_LINEA2(8);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//MOVEMOS ZOMBI DE CUBETA
+		POS_LINEA2(7);
+		ENVIA_DATO(1);
+		//BORRAMOS ZOMBI DE CUBETA2
+		POS_LINEA1(9);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//MOVEMOS ZOMBI DE CUBETA 2
+		POS_LINEA1(8);
+		ENVIA_DATO(1);
+		_delay_ms(500);
+		
+		//BORRAMOS DISPARO PLANTA 2
+		POS_LINEA2(5);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//BORRAMOS ZOMBI CUBETA
+		POS_LINEA2(7);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//DISPARA PLANTA
+		POS_LINEA2(6);
+		ENVIA_DATO(7);
+		//BORRAMOS DISPARO PLANTA 4
+		POS_LINEA1(3);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//DISPARA PLANTA
+		POS_LINEA1(4);
+		ENVIA_DATO(4);
+		//BORRAMOS DISPARO PLANTA 1
+		POS_LINEA2(3);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//DISPARA PLANTA1
+		POS_LINEA2(4);
+		ENVIA_DATO(4);
+		//BORRAMOS ZOMBI DE CUBETA2
+		POS_LINEA1(8);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//MOVEMOS ZOMBI DE CUBETA 2
+		POS_LINEA1(7);
+		ENVIA_DATO(1);
+		_delay_ms(500);
 		
 		
-		
-		
+		//BORRAMOS DISPARO PLANTA 2
+		POS_LINEA2(6);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//BORRAMOS DISPARO PLANTA 2
+		POS_LINEA2(4);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//BORRAMOS ZOMBI CUBETA
+		POS_LINEA2(5);
+		ENVIA_DATO(5);
+		//BORRAMOS DISPARO PLANTA 4
+		POS_LINEA1(4);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//DISPARA PLANTA
+		POS_LINEA1(5);
+		ENVIA_DATO(4);
+		//BORRAMOS ZOMBI DE CUBETA2
+		POS_LINEA1(7);
+		ENVIA_DATO(' ');
+		_delay_ms(10);
+		//MOVEMOS ZOMBI DE CUBETA 2
+		POS_LINEA1(6);
+		ENVIA_DATO(1);
 		_delay_ms(500);
 		
 		
 		
 		
 		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+					
 		
 	}
 }
