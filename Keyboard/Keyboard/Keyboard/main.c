6a810983a4279f18cb2ef21f7f425d8c2ff783ec
Quike <enriquelerdo010@hotmail.com>
2025-09-23 12:29:39 -0600

    First key

--- Keyboard/Keyboard/Keyboard/main.c
+++ Keyboard/Keyboard/Keyboard/main.c
@@ -1,61 +1,38 @@
-
-#define F_CPU 1000000
 #include <avr/io.h>
 #include <util/delay.h>
 #include "LCD.h"
-#include "Keyboard.h" 
+#include "Keyboard.h"
 
 char mensaje[] = "Practica 3";
 char mensaje2[] ="Teclado";
 
-
 int main(void)
 {
-	// Configura PORTC como salida (todos los pines)
 	DDRC = 0xFF;
-	DDRD = 0x0F;
-	PORTD = 0b00001111;
-
-	// Inicializa el LCD
+	DDRD = 0xF0;
+	
 	LCD_INICIALIZA();
-	char tecla = ' ';
-	
-	// Limpia la pantalla y coloca el cursor en la posición inicial
 	LIMPIA_LCD();
 
-	// Escribe un mensaje de bienvenida
-	POS_LINEA1(0); // Cursor en línea 1, posición 0
+	POS_LINEA1(0);
 	ENVIA_CADENA(mensaje);
-	POS_LINEA2(0); // Cursor en línea 1, posición 0
+	POS_LINEA2(0);
 	ENVIA_CADENA(mensaje2);
-	_delay_ms(200);
-
-	CAR_ESP0();
-	CAR_ESP1();
-	CAR_ESP2();
-	CAR_ESP3();
-	CAR_ESP4();
-	CAR_ESP5();
-	CAR_ESP6();
-	CAR_ESP7();
+	_delay_ms(800);
 
 	LIMPIA_LCD();
-	
+
+	char tecla = ' ';
 	while (1)
 	{
-		
 		tecla = LEE_TECLADO();
-		
-		ENVIA_CADENA(tecla);	
-		_delay_ms(100);
-		
-		
-		
-		
+		if (tecla != ' ')
+		{
+			ENVIA_DATO(tecla);
+			while(LEE_TECLADO() != ' '); // Espera a que se suelte
+			_delay_ms(100);
+		}
+		BARRE_TECLADO();
+		_delay_ms(5);
 	}
 }
-		
-		
-		
-		
-		
