5d9b64683f5a284f80a4bc830b295c34e2741aca
QuikeLRD <enriquelerdo010@hotmail.com>
2025-10-24 18:56:44 -0600

    PRACTICAS TIMER AND INTERRUPT FINISHED

---
+++ Musica/Musica/main.c
@@ -0,0 +1,21 @@
+//PRACTICA 6 CONTADOR CTC
+/*QUE NECESITO PARA HACER MUSICA?
+NECESITO TENER LA FRECUENCIA DE CADA NOTA
+*/
+
+
+#include <xc.h>
+#include <avr/io.h>
+
+
+
+
+int main(void)
+{
+    while(1)
+    {
+        
+				
+		 
+    }
+}
