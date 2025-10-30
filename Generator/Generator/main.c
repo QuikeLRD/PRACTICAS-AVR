8eae2fd46a590411c827d11fc17057bfb09284db
Luis Enrique Lerdo <enriquelerdo010@hotmail.com>
2025-10-30 13:00:31 -0600

    Generator Finished

--- Generator/Generator/main.c
+++ Generator/Generator/main.c
@@ -23,63 +23,83 @@ ISR(TIMER0_COMP_vect){
 
 int main(void)
 {
-	
-	//CONFIGURAMOS ENTRADAS Y SALIDAS	
-	DDRB |= (1 <<PB3);																	//PB3 OUTPUT
-	PORTB &= ~(1 << PB3);																//PB3 CLEAN
-	
-	DDRA &= ~((1 << PA0) | (1 << PA1));													//PA0 AND PA1 DEFINED AS INPUTS
-	
-	TCCR0 = 0;																			//CLEAN IT
-	TCCR0 |= (1 << WGM01)|(1 << COM00);													//CTC MODE AND TOGGLE
-	TCCR0 |= (0 << CS02)|(1 << CS01)|(1 << CS00	);										//PRESCALER EN 64
-	TCNT0 = 0;			
-	//MODO CTC
-														
-	OCR0 = 61;	
-	
 	//VALORES DEL OCR
 	const uint8_t OCR_1KHZ	 = 61;
 	const uint8_t OCR_10KHZ	 = 49;
 	const uint8_t OCR_100KHZ =  4;
 	
-	OCR0 = OCR_1KHZ;																	//VALOR INICIAL DEL OCR 
-	TCNT0 = 0;																			//LIMPIAMOS EL REGISTRO	
+	uint8_t selec;
+	uint8_t selec_anterior = 0xFF;
 	
+	//CONFIGURAMOS ENTRADAS Y SALIDAS	
+	DDRB |= (1 <<PB3);																	//PB3 OUTPUT
+	PORTB &= ~(1 << PB3);																//PB3 CLEAN
+	
+	
+	DDRA &= ~(1 << DDA0);																//PA0 AS INPUT
+	DDRA &= ~(1 << DDA1);																//PA1 AS INPUT
+	
+	
+	TCNT0 = 0;																			//CLEAN REGISTER TCNT0
+	OCR0 = 61;																			//INITIAL VALUE OCR0
+																				
+	TCCR0 = (1 << WGM01)|(0 << WGM00)|(0 << COM01)|(1 << COM00)|						//CTC MODE AND TOGGLE
+			(0 << CS02)|(1 << CS01)|(1 << CS00);										//PRESCALER EN 64
+						
+	
+		
 	while(1)
     {
 		
-		uint8_t bit0 = (PINA & (1 << PA0)) ? 1 : 0;
-		uint8_t bit1 = (PINA & (1 << PA1)) ? 1 : 0;
-		uint8_t selec = (bit1 << 1) | bit0; // A1 A0 binario
-			
+		selec = 0;
+						
+		if(!(PINA & (1 << PINA1))){																					//SI PA1 ESTA LOW (CERRADO A GND)
+			selec +=2;
+		}
+		if (!(PINA & (1 << PINA0))){																				//SI PA0 ESTA LOW
+			selec += 1;
+		}
+		
+		
+		if(selec != selec_anterior)
+		{
+					
 			switch(selec){
 				
 				
 			case 1:
-				OCR0 = OCR_1KHZ;														//CONFIGURA EL 0CR0
-				TCCR0 |= (0 << CS02)|(1 << CS01)|(1 << CS00	);							//PRESCALER IN 64 
-				TCNT0 = 0;
-																						//1KHZ
-			break;
+				TCCR0 = (1 << WGM01) | (1 << COM00) | (0 << CS02) | (1 << CS01) | (1 << CS00);						//PRESCALER IN 64 
+				OCR0 = OCR_1KHZ;																					//CONFIGURA EL 0CR0
+				break;																								//1KHZ
+			
 			
 			case 2:
-				OCR0 =OCR_10KHZ;
 				
-				TCCR0 |= (0 << CS02)|(1 << CS01)|(0 << CS00	);							//PRESCALER IN 8
-				TCNT0 = 0;
-																				//10KHZ 
-			break;
+				TCCR0 = (1 << WGM01) | (1 << COM00) | (0 << CS02) | (1 << CS01) | (0 << CS00);						//PRESCALER 8				
+				OCR0 =OCR_10KHZ;
+				break;	
+						
 			
 			case 3:
+				
+				TCCR0 = (1 << WGM01) | (1 << COM00) | (0 << CS02) | (1 << CS01) | (0 << CS00);						//PRESCALER 8
 				OCR0 = OCR_100KHZ;
-				TCCR0 |= (0 << CS02)|(1 << CS01)|(0 << CS00	);							//PRESCALER IN 8
-				TCNT0 = 0;
-			break;
-			
+				break;	
 			default:
-			break;																//100KHZ
+				TCCR0 = (1 << WGM01) | (1 << COM00) | (0 << CS02) | (1 << CS01) | (1 << CS00);						//PRESCALER 64
+				OCR0 = OCR_1KHZ;
+				break;															
 			}
-			_delay_ms(15);        
+			TCNT0 = 0;																								//REINICIAR EL CONTADOR AL CAMBIAR DE MODO, AYUDA A SINCRONIZAR
+			selec_anterior = selec;																					//GUARDA EL ESTADO ACTUAL PARA LA PROXIMA COMPARACIÓN
+		}
+			_delay_ms(20);																							//PEQUEÑO DELAY PARA NO SATURAR LA CPU
     }
 }
+
+//CASE 3: 1KHZ			R= 1.2K, 2R= 2.4k C= 100nF 2C =200nF 0.5C = 50nF
+//CASE 1: 10KHZ			R= 1.5K, 2R= 3.3k C= 10nF  2C = 20nF 0.5C = 5nF
+//CASE 0: 100KHZ		R= 1.5K, 2R= 3.3k C= 1nF   2C = 2nF  0.5C = 0.5nF
+
+//CASE 2: 1KHZ
+
