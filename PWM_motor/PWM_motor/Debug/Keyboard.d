6a9b0c19fd3575f758b9ad4259aeefe004e660b7
Luis Enrique Lerdo <enriquelerdo010@hotmail.com>
2025-11-05 17:41:43 -0600

    Code DC motor

--- PWM_motor/PWM_motor/Debug/Keyboard.d
+++ PWM_motor/PWM_motor/Debug/Keyboard.d
@@ -1,4 +1,4 @@
-main.d main.o: .././main.c \
+Keyboard.d Keyboard.o: .././Keyboard.c \
  c:\program\ files\microchip\xc8\v2.36\avr\avr\include\avr\io.h \
  c:\program\ files\microchip\xc8\v2.36\avr\avr\include\avr\sfr_defs.h \
  c:\program\ files\microchip\xc8\v2.36\avr\avr\include\inttypes.h \
@@ -12,14 +12,7 @@ main.d main.o: .././main.c \
  c:\program\ files\microchip\xc8\v2.36\avr\avr\include\avr\common.h \
  c:\program\ files\microchip\xc8\v2.36\avr\avr\include\avr\fuse.h \
  c:\program\ files\microchip\xc8\v2.36\avr\avr\include\avr\lock.h \
- c:\program\ files\microchip\xc8\v2.36\avr\avr\include\avr\interrupt.h \
- c:\program\ files\microchip\xc8\v2.36\avr\avr\include\util\delay.h \
- c:\program\ files\microchip\xc8\v2.36\avr\avr\include\util\delay_basic.h \
- c:\program\ files\microchip\xc8\v2.36\avr\avr\include\math.h \
- c:\program\ files\microchip\xc8\v2.36\avr\avr\include\stdio.h \
- c:\program\ files\microchip\xc8\v2.36\avr\avr\include\stdlib.h \
- c:\program\ files\microchip\xc8\v2.36\avr\avr\include\string.h \
- .././LCD.h .././Keyboard.h
+ .././Keyboard.h
 
 c:\program\ files\microchip\xc8\v2.36\avr\avr\include\avr\io.h:
 
@@ -46,21 +39,5 @@ c:\program\ files\microchip\xc8\v2.36\avr\avr\include\avr\common.h:
 c:\program\ files\microchip\xc8\v2.36\avr\avr\include\avr\fuse.h:
 
 c:\program\ files\microchip\xc8\v2.36\avr\avr\include\avr\lock.h:
-
-c:\program\ files\microchip\xc8\v2.36\avr\avr\include\avr\interrupt.h:
-
-c:\program\ files\microchip\xc8\v2.36\avr\avr\include\util\delay.h:
-
-c:\program\ files\microchip\xc8\v2.36\avr\avr\include\util\delay_basic.h:
-
-c:\program\ files\microchip\xc8\v2.36\avr\avr\include\math.h:
-
-c:\program\ files\microchip\xc8\v2.36\avr\avr\include\stdio.h:
-
-c:\program\ files\microchip\xc8\v2.36\avr\avr\include\stdlib.h:
-
-c:\program\ files\microchip\xc8\v2.36\avr\avr\include\string.h:
-
-.././LCD.h:
 
 .././Keyboard.h:
